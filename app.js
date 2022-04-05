a2bbfa26c3d9662bbda9049343af72195473d084
sravya453 <100765619+sravya453@users.noreply.github.com>
2022-04-05 14:37:23 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,32 @@
+const app = require('express')();
+const {Server} = require('socket.io');
+
+async function server(){
+    const http = require('http').createServer(app);
+
+	app.get('/', function (req, res) {
+	  res.send('hello world')
+	})
+
+    const io = new Server(http, {transports: ['websocket']});
+    const roomName = 'dogfoot';
+    io.on('connection', (socket) => {
+       socket.on('join', () =>{
+         socket.join(roomName);
+         socket.to(roomName).emit('joined');
+       });
+       socket.on('offer', (offer) => {
+           socket.to(roomName).emit('offer', offer);
+        });
+        socket.on('answer', (answer) => {
+            socket.to(roomName).emit('answer', answer);
+        });
+        socket.on('ice', (ice) => {
+            socket.to(roomName).emit('ice', ice);
+        });
+
+    });
+    http.listen(3000, () => console.log('server open !!'));
+}
+
+server();
